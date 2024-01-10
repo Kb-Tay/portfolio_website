d9e02ae90ca771038a44ecf834b26fe0ce85c2fc
Kb-tay <taykaiboon12@gmail.com>
2024-01-10 23:04:30 +0800

    react pdf bugs

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -5,10 +5,7 @@ import pdf from "../../assets/Aaron.pdf"
 
 import { Document, Page, pdfjs } from 'react-pdf';
 
-pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-  'pdfjs-dist/build/pdf.worker.min.js',
-  import.meta.url,
-).toString();
+pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/legacy/build/pdf.worker.min.js`;
 
 function Resume() {
   const [width, setWidth] = useState(1200);
@@ -21,7 +18,7 @@ function Resume() {
   return (
     <div className='flex justify-center'>
         <Document file={pdf}>
-          <Page renderTextLayer={false}
+          <Page  renderTextLayer={false}
             renderAnnotationLayer={false}
             customTextRenderer={false} 
             pageNumber={1} scale={width > 786 ? 1.7 : 0.6}/>
