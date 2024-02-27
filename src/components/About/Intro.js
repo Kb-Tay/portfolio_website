d7fa4497dd4b67783022b74d7b56dabd8ac267f6
Kb-tay <taykaiboon12@gmail.com>
2024-02-27 10:16:47 +0800

    updates

--- src/components/About/Intro.js
+++ src/components/About/Intro.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 import { AiOutlineLinkedin, AiOutlineInstagram } from "react-icons/ai";
 import { FaGithubSquare } from "react-icons/fa";
+import { Link } from 'react-router-dom';
 
 function Intro() {
   return (
@@ -10,9 +11,12 @@ function Intro() {
       <p class="pt-4">I am the most fluent in Java and have experience coding with Python, Javascript, Typescript.</p>
       <p class="pt-4">Feel free to contact me on...</p>
       <div class="flex flex-row space-x-4 pt-2">
-        <FaGithubSquare class="icon"/>
-        <AiOutlineLinkedin class="icon"/>
-        <AiOutlineInstagram class="icon"/>
+        <a href="https://github.com/Kb-Tay">
+          <FaGithubSquare class="icon"/>
+        </a>
+        <a href="www.linkedin.com/in/aaron-tay-kai-boon">
+          <AiOutlineLinkedin class="icon"/>
+        </a>
       </div>
     </div>
   )
--- src/components/Projects/Project.js
+++ src/components/Projects/Project.js
@@ -16,22 +16,13 @@ function Project() {
         <h1 class="h1 text-center">My Projects</h1>
         <div class="grid sm:grid-cols-2 lg:grid-cols-3 gap-6 pt-10">
           <ProjectCard
-            imgPath = {MoneyManager}
-            imgName = "Money Manager"
-            title = "Money Manager App"
-            content = "Application that I created to manage finances between friends for a internal school hackathon. This app allows you to include your friends in your expenses and notifies them on their dashboard that they owe you expenses."
+            imgPath = {BigAtHeart}
+            imgName = "BigAtHeart"
+            title = "BigAtHeart"
+            content = "A fullstack web application developed to help the voluntary organisation Big At Heart efficiently organise their information. The application achieves this by allowing the organisers to post and create custom sign up forms on the UI and also easily track attendances during events with QR codes."
             hasDemo = {true}
-            gitLink = "https://github.com/Kb-Tay/money_manager"
-            demoLink = "https://moneymanager-app.vercel.app/"
-          />
-          <ProjectCard
-            imgPath = {EusoffApp}
-            imgName = "EusoffApp"
-            title = "Eusoff Hall App"
-            content = "A web application aiming to solve hall problems. The website is currently being used to handle jersey bidding for over 400 hall students. New features are being added such as room bidding and hall games score updates"
-            hasDemo = {false}
-            gitLink = "https://github.com/eusoff-hackers/eusoff-website-23-24"
-            demoLink = ""
+            gitLink = "https://github.com/sopa301/h4g"
+            demoLink = "https://h4g.vercel.app/"
           />
           <ProjectCard
             imgPath = {Unofas}
@@ -43,13 +34,22 @@ function Project() {
             demoLink = "https://github.com/AY2324S1-CS2103T-F12-1/tp/releases/tag/v1.4"
           />
           <ProjectCard
-            imgPath = {BigAtHeart}
-            imgName = "BigAtHeart"
-            title = "BigAtHeart"
-            content = "A fullstack web application developed to help the voluntary organisation Big At Heart efficiently organise their information. The application achieves this by allowing the organisers to post and create custom sign up forms on the UI and also easily track attendances during events with QR codes."
+            imgPath = {EusoffApp}
+            imgName = "EusoffApp"
+            title = "Eusoff Hall App"
+            content = "A web application aiming to solve hall problems. The website is currently being used to handle jersey bidding for over 400 hall students. New features are being added such as room bidding and hall games score updates"
+            hasDemo = {false}
+            gitLink = "https://github.com/eusoff-hackers/eusoff-website-23-24"
+            demoLink = ""
+          />
+          <ProjectCard
+            imgPath = {MoneyManager}
+            imgName = "Money Manager"
+            title = "Money Manager App"
+            content = "Application that I created to manage finances between friends for a internal school hackathon. This app allows you to include your friends in your expenses and notifies them on their dashboard that they owe you expenses."
             hasDemo = {true}
-            gitLink = "https://github.com/sopa301/h4g"
-            demoLink = "https://h4g.vercel.app/"
+            gitLink = "https://github.com/Kb-Tay/money_manager"
+            demoLink = "https://moneymanager-app.vercel.app/"
           />
         </div>
       </div>
